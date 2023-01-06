435d39bc79ca7b1eb869001238aade156fdea843
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-01-06 19:43:53 +0900

    Count-Servers

--- questions/Medium Problems/countServers.js
+++ questions/Medium Problems/countServers.js
@@ -1,28 +1,35 @@
 const countServers = (grid) => {
-  console.log('grid', grid);
-  let serverCount = 0;
-  // getting server count per row and column
-  let serverRowCount = new Array(grid.length).fill(0);
-  let serverColumnCount = new Array(grid[0].length).fill(0);
+  // getting the number of servers
+  let serverRows = new Array(grid.length).fill(0);
+  let serverCols = new Array(grid[0].length).fill(0);
 
   for (let i = 0; i < grid.length; i++) {
     let row = grid[i];
     for (let j = 0; j < row.length; j++) {
       if (grid[i][j] === 1) {
-        serverRowCount[i]++;
-        serverColumnCount[j]++;
+        serverRows[i]++;
+        serverCols[j]++;
       }
     }
   }
-  console.log(serverColumnCount, serverRowCount);
-
+  let serverCount = 0;
+  // checking for connections
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        if (serverRows[i] > 1 || serverCols[j] > 1) {
+          serverCount++;
+        }
+      }
+    }
+  }
   return serverCount;
 };
 // to be continued
 
 // console.log(
 //   countServers([
-//     [1, 1, 0, 0],
+//     [1, 1, 1, 0],
 //     [0, 0, 1, 0],
 //     [0, 0, 1, 0],
 //     [0, 0, 0, 1],
