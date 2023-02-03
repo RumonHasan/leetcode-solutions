dd1926e80d2035190b02a01e5b3c00e2ac0fc3e8
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-02-03 22:22:13 +0900

    maxBalloonsOptimized

---
+++ questions/Easy Problems/maxBalloon.js
@@ -0,0 +1,51 @@
+const maxBalloons = (text) => {
+  // making as many balloons as possible
+  const timeLimitExceeded = () => {
+    let count = 0;
+    while (map.has('b')) {
+      let string = '';
+      for (let index in target) {
+        if (map.has(target[index])) {
+          map.set(target[index], map.get(target[index]) - 1);
+          // preventing the map from having negative occurence
+          if (map.get(target[index]) === 0) {
+            map.delete(target[index]);
+          }
+          string += target[index];
+        } else {
+          map.delete(target[index]);
+        }
+      }
+      if (string === target) count++;
+    }
+    return count;
+  };
+  // optimized using double hash
+  const target = 'balloon';
+  const targetHash = {
+    b: 1,
+    a: 1,
+    l: 2,
+    o: 2,
+    n: 1,
+  };
+  const checkHash = {};
+  for (let index in text) {
+    if (text[index] in targetHash && checkHash[text[index]]) {
+      checkHash[text[index]]++;
+    } else if (text[index] in targetHash && !checkHash[text[index]]) {
+      checkHash[text[index]] = 1;
+    }
+  }
+  // dividing the o and l letters by 2
+  for (let index in target) {
+    if (!checkHash[target[index]]) return 0;
+  }
+  checkHash['o'] = Math.floor(checkHash['o'] / 2);
+  checkHash['l'] = Math.floor(checkHash['l'] / 2);
+  // get the values
+  let objectValues = Object.values(checkHash);
+  return Math.min(...objectValues);
+};
+
+//console.log(maxBalloons('loonbalxballpoon'));
