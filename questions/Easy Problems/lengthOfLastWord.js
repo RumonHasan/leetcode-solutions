4cc7f644442ac4753c618d0f571aa14dd2308523
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-02-23 20:00:27 +0900

    lengthOfWord

---
+++ questions/Easy Problems/lengthOfLastWord.js
@@ -0,0 +1,13 @@
+const lengthOfLastWord = (s) => {
+  const stringReverse = s.split(' ').reverse();
+  let length = 0;
+  for (let index in stringReverse) {
+    if (stringReverse[index].length > 0) {
+      length = stringReverse[index].length;
+      break;
+    }
+  }
+  return length;
+};
+
+console.log(lengthOfLastWord('   fly me   to   the moon  '));
