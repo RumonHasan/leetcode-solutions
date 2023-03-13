08d7d84b4da37129638eaebd0cd10ab7b07a51ee
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-03-14 00:21:59 +0900

    longestWord

---
+++ questions/Medium Problems/longestWord.js
@@ -0,0 +1,36 @@
+const longestWord = (words) => {
+  let sortedWords = words.sort();
+  const checkWord = (word) => {
+    let wordSet = new Set();
+    let string = '';
+    for (let index in word) {
+      string += word[index];
+      wordSet.add(string);
+    }
+    let setArray = new Array(...wordSet);
+    for (let word of setArray) {
+      if (!sortedWords.includes(word)) {
+        return false;
+      }
+    }
+    return true;
+  };
+  let resultArray = [];
+  let maxLen = 0;
+  for (let index = 0; index < sortedWords.length; index++) {
+    let word = sortedWords[index];
+    if (checkWord(word)) {
+      maxLen = Math.max(word.length, maxLen);
+      resultArray.push(word);
+    }
+  }
+  let finalArray = [];
+  for (let index in resultArray) {
+    resultArray[index].length === maxLen && finalArray.push(resultArray[index]);
+  }
+  return finalArray[0] === undefined ? '' : '';
+};
+
+console.log(
+  longestWord(['a', 'banana', 'app', 'appl', 'ap', 'apply', 'apple'])
+);
--- questions/general/yaml.js
+++ questions/general/yaml.js
@@ -43,230 +43,88 @@ const checkYaml = () => {
   console.log(currentVersion);
 };
 
-const jsonObject = [
-  {
-    label: 'Send Survey',
-    status: 'success',
-    root: true,
-    icon: 'list',
-    id: 1,
-    children: [
-      {
-        label: 'Delay',
-        type: 'delay',
-        status: 'success',
-        icon: 'clock',
-        active: false,
-        id: 2,
-        children: [
-          {
-            label: 'Email',
-            type: 'email',
-            status: 'success',
-            icon: 'email',
-            active: false,
-            id: 3,
-            children: [
-              {
-                label: 'Condition',
-                type: 'condition',
-                status: 'success',
-                icon: 'condition',
-                active: false,
-                id: 4,
-                children: [
-                  {
-                    label: 'true',
-                    icon: 'check',
-                    type: 'condition-option',
-                    id: 5,
-                    children: [
-                      {
-                        label: 'Contact Exit',
-                        icon: 'contact-exit',
-                        leaf: true,
-                      },
-                    ],
-                  },
-                  {
-                    label: 'false',
-                    icon: 'close',
-                    type: 'condition-option',
-                    children: [
-                      {
-                        label: 'Contact Exit',
-                        icon: 'contact-exit',
-                        leaf: true,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-];
-
-// const removeChildNodes = (jsonObject, deletionId) => {
-//   let previousId = '';
-//   function findById(obj, id) {
-//     if (obj.id === id) {
-//       return obj;
-//     }
-//     if (obj.children) {
-//       for (var i = 0; i < obj.children.length; i++) {
-//         const result = findById(obj.children[i], id);
-//         if (result) {
-//           return result;
-//         }
-//       }
-//     }
-//     return null;
-//   }
-//   let extractedChild = findById(jsonObject, deletionId).children;
-//   const reAttachChildren = (mainObject, extractedChildren, precedingId) => {
-//     if (mainObject.id === precedingId) {
-//       return mainObject;
-//     }
-//     if (mainObject.children) {
-//       for (let index = 0; index < mainObject.children.length; index++) {
-//         const result = reAttachChildren(
-//           mainObject.children[index],
-//           extractedChildren,
-//           precedingId
-//         );
-//         if (result) {
-//           let precedingIdChild = result.children;
-//           precedingIdChild = extractedChildren;
-//           result.children = precedingIdChild;
-//           console.log(result);
-//           return result;
-//         }
-//       }
-//     }
-//   };
-//   const result = reAttachChildren(jsonObject, extractedChild, deletionId - 1);
-//   console.log(result);
-//   return jsonObject;
-// };
-
-// const newObject = removeChildNodes(jsonObject, 2);
-// console.log(newObject);
-
-// function deleteAndAttachChildren(obj, parentId, idToDelete) {
-//   // if the id is found then perform the deletion
-//   if (obj.id === parentId && obj.children) {
-//     let indexToDelete = null;
-//     for (var i = 0; i < obj.children.length; i++) {
-//       if (obj.children[i].id === idToDelete) {
-//         indexToDelete = i;
-//       }
-//     }
-//     if (indexToDelete !== null) {
-//       let deletedChildren = obj.children.splice(indexToDelete, 1)[0].children;
-//       if (deletedChildren) {
-//         obj.children = obj.children.concat(deletedChildren);
-//       }
-//     }
-//   } else if (obj.children) {
-//     for (var i = 0; i < obj.children.length; i++) {
-//       deleteAndAttachChildren(obj.children[i], parentId, idToDelete);
-//     }
-//   }
-//   return obj;
-// }
-
-// var modifiedObj = deleteAndAttachChildren(jsonObject, 'hello', 'bye');
-// console.log(modifiedObj);
-
-// function removeChildWithId(node, idToRemove) {
-//   if (!node.children) {
-//     return;
-//   }
-//   let newNode = null;
-//   for (let i = 0; i < node.children.length; i++) {
-//     const child = node.children[i];
-//     const deletedChildChildren = child.children;
-//     if (child.id === idToRemove) {
-//       node.children.splice(i, 1);
-//       if (deletedChildChildren) {
-//         node.children.splice(i, 0, ...deletedChildChildren);
-//       }
-//       return;
-//     }
-//     removeChildWithId(child, idToRemove);
-//   }
-// }
-// console.log(removeChildWithId(jsonObject, 3));
-function deleteNodeAndAddChildren(node) {
-  if (node.id === 4) {
-    const index = node.parent.children.findIndex(
-      (child) => child.id === node.id
-    );
-    if (index !== -1) {
-      node.parent.children.splice(index, 1);
-      node.parent.children[0].children.push(...node.children);
-    }
+const jsonObject = {
+  label: 'Send Survey',
+  status: 'success',
+  root: true,
+  icon: 'list',
+  id: 1,
+  children: [
+    {
+      label: 'Delay',
+      type: 'delay',
+      status: 'success',
+      icon: 'clock',
+      active: false,
+      id: 2,
+      children: [
+        {
+          label: 'Email',
+          type: 'email',
+          status: 'success',
+          icon: 'email',
+          active: false,
+          id: 3,
+          children: [
+            {
+              label: 'Condition',
+              type: 'condition',
+              status: 'success',
+              icon: 'condition',
+              active: false,
+              id: 4,
+              children: [
+                {
+                  label: 'true',
+                  icon: 'check',
+                  type: 'condition-option',
+                  id: 5,
+                  children: [
+                    {
+                      label: 'Contact Exit',
+                      icon: 'contact-exit',
+                      leaf: true,
+                    },
+                  ],
+                },
+                {
+                  label: 'false',
+                  icon: 'close',
+                  type: 'condition-option',
+                  children: [
+                    {
+                      label: 'Contact Exit',
+                      icon: 'contact-exit',
+                      leaf: true,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+const removeChildrenNodes = (jsonObject, deleteIndex) => {
+  let data = jsonObject;
+  let prevChildren = data.children;
+  // only for first child
+  if (prevChildren[0].id === deleteIndex) {
+    let nextChild = prevChildren[0].children;
+    data.children = nextChild;
+    return data;
   } else {
-    node.children = node.children.map((child) =>
-      deleteNodeAndAddChildren(child)
-    );
-  }
-  return node;
-}
-
-// function removeChildrenWithId(array, idToRemove) {
-//   for (let i = 0; i < array.length; i++) {
-//     const obj = array[i];
-//     2;
-
-//     if (obj.children) {
-//       removeChildrenWithId(obj.children, idToRemove);
-//     }
-
-//     // remove child object with matching id
-//     for (let j = 0; j < obj.children.length; j++) {
-//       const child = obj.children[j];
-
-//       if (child.id === idToRemove) {
-//         obj.children.splice(j, 1);
-
-//         // append child object's children to parent object's children array
-//         if (child.children) {
-//           obj.children.splice(j, 0, ...child.children);
-//         }
-
-//         j--;
-//       }
-//     }
-//   }
-// }
-
-// console.log(removeChildrenWithId(jsonObject, 3));
-
-function deleteObjectById(objArr, idToDelete) {
-  for (let i = 0; i < objArr.length; i++) {
-    const obj = objArr[i];
-    if (obj.id === idToDelete) {
-      const children = obj.children;
-      objArr.splice(i, 1);
-      if (children) {
-        objArr.splice(i, 0, ...children);
+    // remaining children
+    while (data.children) {
+      let nextChildren = data.children[0].children;
+      if (nextChildren[0].id === deleteIndex) {
+        data.children[0].children = nextChildren[0].children;
+        return data;
       }
-      return objArr;
-    } else if (obj.children) {
-      const updatedChildren = deleteObjectById(obj.children, idToDelete);
-      if (updatedChildren) {
-        obj.children = updatedChildren;
-        return objArr;
-      }
+      data.children = data.children[0].children;
     }
   }
-  return null;
-}
-
-const updatedJsonObject = deleteObjectById(jsonObject, 2);
-console.log(updatedJsonObject);
+};
+// console.log(removeChildrenNodes(jsonObject, 3));
