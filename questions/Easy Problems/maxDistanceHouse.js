8896ff980b9a96df5e1c539851a61f55d42386dc
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-03-25 18:51:24 +0900

    maxDistanceHouse

---
+++ questions/Easy Problems/maxDistanceHouse.js
@@ -0,0 +1,22 @@
+const maxDistanceHouse = (colors) => {
+  let maxDistance = 0;
+  for (let index = 0; index < colors.length; index++) {
+    let checkHouseIndex = index;
+    for (
+      let secondIndex = index + 1;
+      secondIndex < colors.length;
+      secondIndex++
+    ) {
+      let houseIndex = secondIndex;
+      if (colors[index] !== colors[secondIndex]) {
+        maxDistance = Math.max(
+          maxDistance,
+          Math.abs(checkHouseIndex - houseIndex)
+        );
+      }
+    }
+  }
+  return maxDistance;
+};
+
+console.log(maxDistanceHouse([1, 8, 3, 8, 3]));
