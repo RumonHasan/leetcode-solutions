85fa3a2420e6d2474faed0b5ea9303ef8e3446ae
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-03-30 19:15:16 +0900

    rotate

---
+++ questions/Medium Problems/rotate.js
@@ -0,0 +1,17 @@
+const rotate = (nums, k) => {
+  if (nums === null || nums.length === 0) {
+    return;
+  }
+  k = k % nums.length;
+  let copy = [...nums];
+  for (let index = 0; index < k; index++) {
+    let element = copy.pop();
+    copy.unshift(element);
+  }
+  for (let index in copy) {
+    nums[index] = copy[index];
+  }
+  return nums;
+};
+// rotate the array to the right by k places
+console.log(rotate([1, 2], 3));
