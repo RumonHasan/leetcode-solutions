e4129e3d39f3e5f37d9aee12cbdff97a96915e25
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-04-07 20:47:35 +0900

    countVowelSubstring

---
+++ questions/Medium Problems/countVowelsOfAllSub.js
@@ -0,0 +1,14 @@
+const countVowelsSubstring = (word) => {
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+  let total = 0;
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.has(word[i])) {
+      count += i + 1;
+    }
+    total += count;
+  }
+  return total;
+};
+
+console.log(countVowelsSubstring('aba'));
