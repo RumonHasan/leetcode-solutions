7dd478c3067b5128af067d2b663338fec7df4926
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-04-08 21:01:54 +0900

    maxLenBetweenEqualChars

---
+++ questions/Easy Problems/maxLengthBetweenEqualCharacters.js
@@ -0,0 +1,26 @@
+const maxLenBetweenEqualChars = (s) => {
+  let distanceObject = {};
+  let distanceMap = new Map();
+  for (let index = 0; index < s.length; index++) {
+    if (distanceObject[s[index]]) {
+      distanceObject[s[index]] = [...distanceObject[s[index]], index];
+    } else {
+      distanceObject[s[index]] = [index];
+    }
+    // if (distanceMap.has(s[index])) {
+    //   distanceMap.get(s[index]).push(index);
+    // } else {
+    //   distanceMap.set(s[index], [index]);
+    // }
+  }
+  let maxLength = -1;
+  for (let [_, value] of Object.entries(distanceObject)) {
+    if (value.length > 1) {
+      const firstIndex = value[0];
+      const lastIndex = value[value.length - 1];
+      maxLength = Math.max(lastIndex - firstIndex - 1, maxLength);
+    }
+  }
+  return maxLength;
+};
+// console.log(maxLenBetweenEqualChars('csdsc'));
