62c7b925ebcf28e25bfb0fcffe3e4ed77ed8721e
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-04-19 20:48:40 +0900

    maxProductLength

---
+++ questions/Medium Problems/maxProductLengthWord.js
@@ -0,0 +1,30 @@
+const maxProduct = (words) => {
+  console.log(words);
+  words.sort((a, b) => b.length - a.length);
+  const checkCommonChars = (word, checkWord) => {
+    for (let index = 0; index < word.length; index++) {
+      if (checkWord.includes(word.charAt(index))) {
+        return false;
+      }
+    }
+    return true;
+  };
+  let maxLength = 0;
+  for (let index = 0; index < words.length; index++) {
+    let mainWord = words[index];
+    for (
+      let secondIndex = index + 1;
+      secondIndex < words.length;
+      secondIndex++
+    ) {
+      if (words[secondIndex].length * mainWord.length > maxLength) {
+        if (checkCommonChars(mainWord, words[secondIndex])) {
+          maxLength = mainWord.length * words[secondIndex].length;
+        }
+      }
+    }
+  }
+  return maxLength;
+};
+
+console.log(maxProduct(['abcw', 'baz', 'foo', 'bar', 'xtfn', 'abcdef']));
