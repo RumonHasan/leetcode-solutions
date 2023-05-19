03aac6f5a480060062056854bced648a8fa86a3e
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-05-19 19:08:14 +0900

    longestIncreasingSubsequence

---
+++ questions/Medium Problems/longestIncreasingSubsequence.js
@@ -0,0 +1,16 @@
+const longestIncreasingSubsequence = (nums) => {
+  let dpArray = new Array(nums.length).fill(1); // setting 1 by default to allow one substring
+  for (let index = 0; index < nums.length; index++) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex++) {
+      if (nums[secondIndex] >= nums[index]) {
+        continue;
+      }
+      if (nums[secondIndex] < nums[index]) {
+        dpArray[index] = Math.max(dpArray[secondIndex] + 1, dpArray[index]);
+      }
+    }
+  }
+  return Math.max(...dpArray);
+};
+// check up to the initial index
+console.log(longestIncreasingSubsequence([2, 5, 3, 7, 101]));
