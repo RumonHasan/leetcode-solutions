69579b01101b34886bfee19823e4229968c62ae6
hasan rumon <hasanrumon@hasans-MacBook-Pro.local>
2023-07-05 09:56:30 +0900

    studentRecordCheck

---
+++ questions/Easy Problems/studentCheckRecord.js
@@ -0,0 +1,28 @@
+const studentCheckRecord = (s) => {
+  let presentCounter = 0;
+  let absentCounter = 0;
+  let lateCheck = false;
+  let index = 0;
+  while (index < s.length) {
+    let localLateCounter = 0;
+    s[index] === 'P' && presentCounter++;
+    s[index] === 'A' && absentCounter++;
+    if (s[index] === 'L') {
+      while (s[index] === 'L' && index < s.length) {
+        localLateCounter++;
+        index++;
+      }
+    } else {
+      index++;
+    }
+    if (localLateCounter >= 3) {
+      lateCheck = true;
+    }
+  }
+  if (absentCounter < 2 && !lateCheck) {
+    return true;
+  }
+  return false;
+};
+
+//console.log(studentCheckRecord('ALLAPPL'));
