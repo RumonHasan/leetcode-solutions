544d15ffcfdfadddc3b270cd35023589b367dfcc
Rumon Mahmudul Hasan <rumonhasan@Rumons-MacBook-Pro.local>
2023-07-15 20:06:29 +0900

    kItemsWithMaxSum

---
+++ questions/Easy Problems/kItemsWithMaximumSum.js
@@ -0,0 +1,36 @@
+const kItemsWithMaximumSum = (numOnes, numZeros, numNegOnes, k) => {
+  let totalSum = 0;
+  const populate = (len, type) => {
+    let subArray = [];
+    if (len > 0) {
+      for (let index = 0; index < len; index++) {
+        switch (type) {
+          case '1':
+            subArray.push(1);
+            break;
+          case '-1':
+            subArray.push(-1);
+            break;
+          case '0':
+            subArray.push(0);
+            break;
+        }
+      }
+    }
+    return subArray;
+  };
+
+  let ones = populate(numOnes, '1');
+  let negs = populate(numNegOnes, '-1');
+  let zeros = populate(numZeros, '0');
+  let array = [...ones, ...zeros, ...negs];
+  let counter = 0;
+  for (let index in array) {
+    if (counter === k) break;
+    totalSum += array[index];
+    counter++;
+  }
+  return totalSum;
+};
+
+console.log(kItemsWithMaximumSum(3, 2, 0, 2));
