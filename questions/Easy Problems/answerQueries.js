77ddb6ddedd6c77d46db9979c0962ecc533598bf
RumonHasan <rumonuniv@gmail.com>
2023-11-04 19:56:52 +0900

    smallerNumbers

--- questions/Easy Problems/answerQueries.js
+++ questions/Easy Problems/answerQueries.js
@@ -19,3 +19,23 @@ const answerQueries = (nums, queries) => {
 };
 // I do not know what I did but it works
 //console.log(answerQueries([4, 5, 2, 1], [3, 10, 21]));
+
+const smallerNumbers = (nums) => {
+  let map = new Map();
+  let copy = [...nums];
+  nums.sort((a, b) => a - b);
+  for (let index = 0; index < nums.length; index++) {
+    if (map.has(nums[index])) continue;
+    map.set(nums[index], index);
+  }
+  let end = 0;
+  let result = [];
+  while (end < copy.length) {
+    const number = copy[end];
+    result.push(map.get(number));
+    end++;
+  }
+  return result;
+};
+
+//console.log(smallerNumbers([8, 1, 2, 2, 3]));
