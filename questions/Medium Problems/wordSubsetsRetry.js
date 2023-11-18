2137df615d0f68a10d1ce73976057c317cdc89aa
RumonHasan <rumonuniv@gmail.com>
2023-11-18 18:55:14 +0900

    wordSubsets

---
+++ questions/Medium Problems/wordSubsetsRetry.js
@@ -0,0 +1,63 @@
+const wordSubsetsRetry = (words1, words2) => {
+  const intuitiveStraightForwardApproach = () => {
+    let result = [];
+    const generateCharFreq = (words2) => {
+      let charMap = new Map();
+      for (let index = 0; index < words2.length; index++) {
+        let word = words2[index];
+        let localCharMap = new Map();
+        for (let subIndex = 0; subIndex < word.length; subIndex++) {
+          const char = word[subIndex];
+          localCharMap.has(char)
+            ? localCharMap.set(char, localCharMap.get(char) + 1)
+            : localCharMap.set(char, 1);
+        }
+        if (index === 0) {
+          charMap = new Map(localCharMap);
+        } else {
+          for (let [key, value] of localCharMap) {
+            if (charMap.has(key) && charMap.get(key) < value) {
+              charMap.set(key, value);
+            }
+            if (!charMap.has(key)) {
+              charMap.set(key, value);
+            }
+          }
+        }
+      }
+      return charMap;
+    };
+    let map = generateCharFreq(words2); // sets max frequency of each character from each word from words2
+    // now to check universality
+    let end = 0;
+    while (end < words1.length) {
+      const word = words1[end];
+      let wordMap = new Map();
+      for (let index = 0; index < word.length; index++) {
+        const wordChar = word[index];
+        wordMap.has(wordChar)
+          ? wordMap.set(wordChar, wordMap.get(wordChar) + 1)
+          : wordMap.set(wordChar, 1);
+      }
+      let mapSize = 0;
+      for (let [key, value] of map) {
+        if (wordMap.has(key) && wordMap.get(key) >= value) {
+          mapSize++;
+        }
+      }
+      if (mapSize === map.size) result.push(word);
+      end++;
+    }
+    return result;
+  };
+};
+// the char frequency should contain the max frequency of each alphabet in the words2
+// creating a frequency map in order to check whether the word in words1 is universal or not if not do not add to the list
+// this is the best approach to avoid TLE brute force approach
+// after creating the char frequency need to iterate through the main list of words and inject the universally accepted words into the final result
+console.log(
+  wordSubsetsRetry(
+    ['amazon', 'apple', 'facebook', 'google', 'leetcode'],
+    ['e', 'o']
+  )
+);
