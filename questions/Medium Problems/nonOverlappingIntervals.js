ec299d0d5926c66287dd131076b38dcdc8d1a340
RumonHasan <rumonuniv@gmail.com>
2023-12-29 17:17:29 +0900

    partlabels

--- questions/Medium Problems/nonOverlappingIntervals.js
+++ questions/Medium Problems/nonOverlappingIntervals.js
@@ -25,12 +25,42 @@ const nonOverlappingIntervals = (intervals) => {
   };
 };
 
-// console.log(
-//   nonOverlappingIntervals([
-//     [0, 2],
-//     [1, 3],
-//     [2, 4],
-//     [3, 5],
-//     [4, 6],
-//   ])
-// );
+// get intervals and try it out
+const partLabels = (s) => {
+  const generateIntervals = (type, string) => {
+    let intervalDS = type === 'map' ? new Map() : {};
+    if (type === 'map') {
+      for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+        if (!intervalDS.has(char)) {
+          intervalDS.set(char, [i, string.lastIndexOf(char)]);
+        }
+      }
+    }
+    return intervalDS;
+  };
+  // or using map
+  let intervals = [...generateIntervals('map', s).values()];
+  //focusing on overlapping intervals in order to find the length
+  let array = [];
+  let end = 1;
+  let currLowerBound = intervals[0][0];
+  let currMaxBound = intervals[0][1];
+  while (end < intervals.length) {
+    const currInterval = intervals[end];
+    if (currMaxBound < currInterval[0]) {
+      array.push(currMaxBound - currLowerBound + 1);
+      currLowerBound = currInterval[0];
+      currMaxBound = currInterval[1];
+    } else if (currMaxBound < currInterval[1]) {
+      currMaxBound = currInterval[1];
+    }
+    if (end === intervals.length - 1) {
+      array.push(currMaxBound - currLowerBound + 1);
+    }
+    end++;
+  }
+  return array;
+};
+
+//console.log(partLabels('ababcbacadefegdehijhklij'));
