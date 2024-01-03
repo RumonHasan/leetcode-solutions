d8d6d36c2ac4bc91686f614e39f3859c26098172
RumonHasan <rumonuniv@gmail.com>
2024-01-03 20:49:12 +0900

    minLnegth

--- questions/Medium Problems/dailyTemperatures.js
+++ questions/Medium Problems/dailyTemperatures.js
@@ -22,3 +22,55 @@ const dailyTemperatures = (temperatures) => {
 };
 
 //console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
+
+const minLength = (s) => {
+  console.log(s);
+  const first = () => {
+    let stack = [];
+    let end = 0;
+    while (end < s.length) {
+      if (end === 0) {
+        stack.push(s[end]);
+        end++;
+        continue;
+      }
+      // check condition
+      if (
+        (s[end] === 'B' && stack[stack.length - 1] === 'A') ||
+        (s[end] === 'D' && stack[stack.length - 1] === 'C' && stack.length)
+      ) {
+        stack.pop();
+      } else {
+        stack.push(s[end]);
+      }
+      end++;
+    }
+
+    return stack.length;
+  };
+
+  const secondDoublePop = () => {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+      const currChar = s[i];
+      if (i === 0) {
+        stack.push(currChar);
+        continue;
+      }
+      const stackLastChar = stack[stack.length - 1];
+      if (
+        (currChar === 'B' && stackLastChar === 'A') ||
+        (currChar === 'D' && stackLastChar === 'C')
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currChar);
+      }
+    }
+    return stack.length;
+  };
+
+  console.log(secondDoublePop());
+};
+
+console.log(minLength('ABFCACDB'));
