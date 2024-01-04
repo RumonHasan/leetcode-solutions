cb0729504eef33859fe773a5da73ea0eadf03562
RumonHasan <rumonuniv@gmail.com>
2024-01-04 20:25:09 +0900

    random eaasy ones

--- questions/Medium Problems/dailyTemperatures.js
+++ questions/Medium Problems/dailyTemperatures.js
@@ -73,4 +73,71 @@ const minLength = (s) => {
   console.log(secondDoublePop());
 };
 
-console.log(minLength('ABFCACDB'));
+// console.log(minLength('ABFCACDB'));
+
+const sumCounts = (nums) => {
+  const basicBruteForceApproach = () => {
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+      let set = new Set();
+      for (let j = i; j < nums.length; j++) {
+        set.add(nums[j]);
+        const setSize = set.size;
+        total += Math.pow(setSize, 2);
+      }
+    }
+    return total;
+  };
+};
+
+console.log(sumCounts([1, 2, 1]));
+
+const findBigram = (text, first, second) => {
+  const array = text.split(' ');
+  let collection = [];
+  let localWindow = [];
+  for (let i = 0; i < 3; i++) {
+    localWindow.push(array[i]);
+  }
+  if (localWindow[0] == first && localWindow[1] == second) {
+    collection.push(localWindow[2]);
+  }
+  let end = 3;
+  while (end < array.length) {
+    localWindow.shift();
+    localWindow.push(array[end]);
+    if (localWindow[0] == first && localWindow[1] == second) {
+      collection.push(localWindow[localWindow.length - 1]);
+    }
+    end++;
+  }
+  return collection;
+};
+
+// console.log(
+//   findBigram('alice is a good girl she is a good student', 'a', 'good')
+// );
+
+const validMountainArray = (arr) => {
+  let end = 0;
+  while (end < arr.length) {
+    if (arr[end] < arr[end + 1]) {
+      let checkOne = false;
+      let checkTwo = false;
+      while (end < arr.length && arr[end] < arr[end + 1]) {
+        checkOne = true;
+        end++;
+      }
+      while (end < arr.length && arr[end] > arr[end + 1]) {
+        checkTwo = true;
+        end++;
+      }
+      if (end !== arr.length - 1) return false;
+      if (checkOne && checkTwo) return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+//console.log(validMountainArray([3, 7, 6, 4, 3, 0, 1, 0]));
