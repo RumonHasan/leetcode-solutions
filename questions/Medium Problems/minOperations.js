84d27e533e3ad07e13f604f58fd6ba805c2b6a33
RumonHasan <rumonuniv@gmail.com>
2024-01-05 20:35:09 +0900

    rowAndMaxOnes

--- questions/Medium Problems/minOperations.js
+++ questions/Medium Problems/minOperations.js
@@ -25,3 +25,33 @@ const minOperations = (nums, x) => {
 };
 
 //console.log(minOperations([1, 1], 3));
+
+const rowAndMaxOnes = (mat) => {
+  const basicBeginnerFriendlyIntuition = () => {
+    let maxRowCount = 0;
+    let rowCountObject = {};
+    for (let i = 0; i < mat.length; i++) {
+      const row = mat[i].length;
+      let oneCount = 0;
+      for (let j = 0; j < row; j++) {
+        const el = mat[i][j];
+        el === 1 && oneCount++;
+      }
+      rowCountObject[i] = oneCount;
+      maxRowCount = Math.max(maxRowCount, oneCount);
+    }
+    for (let [key, value] of Object.entries(rowCountObject)) {
+      if (value === maxRowCount) {
+        return [Number(key), value];
+      }
+    }
+  };
+};
+
+// console.log(
+//   rowAndMaxOnes([
+//     [0, 0],
+//     [1, 1],
+//     [0, 0],
+//   ])
+// );
