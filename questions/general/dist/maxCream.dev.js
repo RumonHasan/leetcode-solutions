2d191fe9a64d810b22bba2b5103e83e82c1a0df6
RumonHasan <rumonuniv@gmail.com>
2024-07-17 21:07:52 +0900

    isWinner

--- questions/general/dist/maxCream.dev.js
+++ questions/general/dist/maxCream.dev.js
@@ -24,3 +24,51 @@ var maxIcecream = function maxIcecream(costs, coins) {
 
   return counter;
 }; //console.log(maxIcecream([1, 3, 2, 4, 1], 7));
+
+
+var backspacecompare = function backspacecompare(s, t) {
+  var backspaceCheck = function backspaceCheck(string) {
+    var stack = [];
+
+    for (var i = 0; i < string.length; i++) {
+      if (stack.length && string[i] === '#') {
+        stack.pop();
+      } else {
+        if (string[i] !== '#') {
+          stack.push(string[i]);
+        }
+      }
+    }
+
+    return stack.join('');
+  };
+
+  return backspaceCheck(s) === backspaceCheck(t);
+}; // bowling score
+
+
+var isWinner = function isWinner(player1, player2) {
+  var getTotal = function getTotal(pins) {
+    var total = 0;
+
+    for (var i = 0; i < pins.length; i++) {
+      var curr = pins[i];
+      var prev = pins[i - 1];
+      var prevprev = pins[i - 2];
+
+      if (prev === 10 || prevprev === 10) {
+        var _double = curr * 2;
+
+        total += _double;
+      } else {
+        total += curr;
+      }
+    }
+
+    return total;
+  };
+
+  var player1Score = getTotal(player1);
+  var player2Score = getTotal(player2);
+  return player1Score > player2Score ? 1 : player2Score > player1Score ? 2 : 0;
+}; //console.log(isWinner([5, 10, 3, 2], [6, 5, 7, 3]));
--- questions/general/maxCream.js
+++ questions/general/maxCream.js
@@ -18,3 +18,45 @@ const maxIcecream = (costs, coins) => {
 };
 
 //console.log(maxIcecream([1, 3, 2, 4, 1], 7));
+
+const backspacecompare = (s, t) => {
+  const backspaceCheck = (string) => {
+    let stack = [];
+    for (let i = 0; i < string.length; i++) {
+      if (stack.length && string[i] === '#') {
+        stack.pop();
+      } else {
+        if (string[i] !== '#') {
+          stack.push(string[i]);
+        }
+      }
+    }
+    return stack.join('');
+  };
+  return backspaceCheck(s) === backspaceCheck(t);
+};
+
+// bowling score
+const isWinner = (player1, player2) => {
+  const getTotal = (pins) => {
+    let total = 0;
+    for (let i = 0; i < pins.length; i++) {
+      const curr = pins[i];
+      const prev = pins[i - 1];
+      const prevprev = pins[i - 2];
+      if (prev === 10 || prevprev === 10) {
+        const double = curr * 2;
+        total += double;
+      } else {
+        total += curr;
+      }
+    }
+    return total;
+  };
+
+  const player1Score = getTotal(player1);
+  const player2Score = getTotal(player2);
+  return player1Score > player2Score ? 1 : player2Score > player1Score ? 2 : 0;
+};
+
+//console.log(isWinner([5, 10, 3, 2], [6, 5, 7, 3]));
