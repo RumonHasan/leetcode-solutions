8fd810f808fd0897a381cd730a6ef611c5d0b50e
RumonHasan <rumonuniv@gmail.com>
2024-11-05 20:08:26 +0900

    subarray practise

--- questions/general/dist/favoriteCompanies.dev.js
+++ questions/general/dist/favoriteCompanies.dev.js
@@ -42,6 +42,74 @@ var favoriteCompanies = function favoriteCompanies(_favoriteCompanies) {
   return indices.sort(function (a, b) {
     return a - b;
   });
-};
+}; // console.log(
+//   favoriteCompanies([
+//     ['leetcode', 'google', 'facebook'],
+//     ['google', 'microsoft'],
+//     ['google', 'facebook'],
+//     ['google'],
+//     ['amazon'],
+//   ])
+// );
 
-console.log(favoriteCompanies([['leetcode', 'google', 'facebook'], ['google', 'microsoft'], ['google', 'facebook'], ['google'], ['amazon']]));
+
+var countSubarrays = function countSubarrays(nums, k) {
+  var max = Math.max.apply(Math, _toConsumableArray(nums)); // check for k times count subarrays
+
+  var end = 0;
+  var count = 0;
+  var subCount = 0;
+  var start = 0;
+
+  while (end < nums.length) {
+    if (nums[end] === max) {
+      count++;
+    }
+
+    while (count === k) {
+      if (nums[start] === max) {
+        count--;
+      }
+
+      start++;
+    }
+
+    subCount += start > 0 ? start : 0;
+    end++;
+  } // main logic is once it hits the required k elements its the entire subarray that becomes a possible subarray so only have to reduce from the start to filter out
+
+
+  return subCount;
+}; //console.log(countSubarrays([1, 3, 2, 3, 3], 2));
+
+
+var completeSubarray = function completeSubarray(nums) {
+  var setSize = new Set(_toConsumableArray(nums)).size;
+  var map = new Map();
+  var end = 0;
+  var start = 0;
+  var size = 0;
+
+  while (end < nums.length) {
+    map.set(nums[end], (map.get(nums[end]) || 0) + 1);
+
+    while (map.size === setSize) {
+      // limits based on equality of size of set and map
+      if (map.has(nums[start])) {
+        map.set(nums[start], map.get(nums[start]) - 1);
+
+        if (map.get(nums[start]) === 0) {
+          map["delete"](nums[start]);
+        }
+      }
+
+      console.log(map);
+      start++;
+    }
+
+    size += start > 0 ? start : 0;
+    end++;
+  }
+
+  return size;
+}; //console.log(completeSubarray([1, 3, 1, 2, 2]));
--- questions/general/favoriteCompanies.js
+++ questions/general/favoriteCompanies.js
@@ -24,12 +24,65 @@ const favoriteCompanies = (favoriteCompanies) => {
   return indices.sort((a, b) => a - b);
 };
 
-console.log(
-  favoriteCompanies([
-    ['leetcode', 'google', 'facebook'],
-    ['google', 'microsoft'],
-    ['google', 'facebook'],
-    ['google'],
-    ['amazon'],
-  ])
-);
+// console.log(
+//   favoriteCompanies([
+//     ['leetcode', 'google', 'facebook'],
+//     ['google', 'microsoft'],
+//     ['google', 'facebook'],
+//     ['google'],
+//     ['amazon'],
+//   ])
+// );
+
+const countSubarrays = (nums, k) => {
+  let max = Math.max(...nums);
+  // check for k times count subarrays
+  let end = 0;
+  let count = 0;
+  let subCount = 0;
+  let start = 0;
+  while (end < nums.length) {
+    if (nums[end] === max) {
+      count++;
+    }
+    while (count === k) {
+      if (nums[start] === max) {
+        count--;
+      }
+      start++;
+    }
+    subCount += start > 0 ? start : 0;
+    end++;
+  }
+  // main logic is once it hits the required k elements its the entire subarray that becomes a possible subarray so only have to reduce from the start to filter out
+  return subCount;
+};
+
+//console.log(countSubarrays([1, 3, 2, 3, 3], 2));
+
+const completeSubarray = (nums) => {
+  let setSize = new Set([...nums]).size;
+  let map = new Map();
+  let end = 0;
+  let start = 0;
+  let size = 0;
+  while (end < nums.length) {
+    map.set(nums[end], (map.get(nums[end]) || 0) + 1);
+    while (map.size === setSize) {
+      // limits based on equality of size of set and map
+      if (map.has(nums[start])) {
+        map.set(nums[start], map.get(nums[start]) - 1);
+        if (map.get(nums[start]) === 0) {
+          map.delete(nums[start]);
+        }
+      }
+      console.log(map);
+      start++;
+    }
+    size += start > 0 ? start : 0;
+    end++;
+  }
+  return size;
+};
+
+//console.log(completeSubarray([1, 3, 1, 2, 2]));
