415023871b1db1f98532d54769ee2c3dcc745b6f
RumonHasan <rumonuniv@gmail.com>
2024-12-18 01:19:11 +0900

    maxscore

--- questions/general/dist/zigzagConversaion.dev.js
+++ questions/general/dist/zigzagConversaion.dev.js
@@ -22,3 +22,47 @@ var zig = function zig(s, numRows) {
 
   return result.join('');
 }; //console.log(zig('AB', 1));
+
+
+var maxScore = function maxScore(s) {
+  var rightOne = new Array(s.length).fill(0);
+  var leftZero = new Array(s.length).fill(0);
+
+  for (var i = 0; i < s.length; i++) {
+    var curr = s[i];
+
+    if (curr === '0') {
+      if (leftZero[i - 1]) {
+        leftZero[i] = leftZero[i - 1] + 1;
+      } else {
+        leftZero[i] = 1;
+      }
+    } else {
+      leftZero[i] = leftZero[i - 1] ? leftZero[i - 1] : leftZero[i];
+    }
+  }
+
+  for (var _i = s.length - 1; _i >= 0; _i--) {
+    var _curr = s[_i];
+
+    if (_curr === '1') {
+      if (rightOne[_i + 1]) {
+        rightOne[_i] = rightOne[_i + 1] + 1;
+      } else {
+        rightOne[_i] = 1;
+      }
+    } else {
+      rightOne[_i] = rightOne[_i + 1] ? rightOne[_i + 1] : rightOne[_i];
+    }
+  }
+
+  var maxScore = 0;
+
+  for (var _i2 = 0; _i2 < s.length - 1; _i2++) {
+    maxScore = Math.max(maxScore, rightOne[_i2 + 1] + leftZero[_i2]);
+  }
+
+  return maxScore;
+};
+
+console.log(maxScore('00'));
--- questions/general/zigzagConversaion.js
+++ questions/general/zigzagConversaion.js
@@ -17,3 +17,40 @@ const zig = (s, numRows) => {
 };
 
 //console.log(zig('AB', 1));
+
+const maxScore = (s) => {
+  let rightOne = new Array(s.length).fill(0);
+  let leftZero = new Array(s.length).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+    if (curr === '0') {
+      if (leftZero[i - 1]) {
+        leftZero[i] = leftZero[i - 1] + 1;
+      } else {
+        leftZero[i] = 1;
+      }
+    } else {
+      leftZero[i] = leftZero[i - 1] ? leftZero[i - 1] : leftZero[i];
+    }
+  }
+  for (let i = s.length - 1; i >= 0; i--) {
+    const curr = s[i];
+    if (curr === '1') {
+      if (rightOne[i + 1]) {
+        rightOne[i] = rightOne[i + 1] + 1;
+      } else {
+        rightOne[i] = 1;
+      }
+    } else {
+      rightOne[i] = rightOne[i + 1] ? rightOne[i + 1] : rightOne[i];
+    }
+  }
+  let maxScore = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    maxScore = Math.max(maxScore, rightOne[i + 1] + leftZero[i]);
+  }
+  return maxScore;
+};
+
+console.log(maxScore('00'));
