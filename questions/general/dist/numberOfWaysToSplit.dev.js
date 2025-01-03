d471464459681908cff497aedd994a8ab2c66648
RumonHasan <rumonuniv@gmail.com>
2025-01-03 19:25:20 +0900

    numberofways to split

---
+++ questions/general/dist/numberOfWaysToSplit.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var numberOfWaysToSplit = function numberOfWaysToSplit(nums) {
+  var n = nums.length; // Build left prefix sum
+
+  var leftPref = [BigInt(nums[0])];
+
+  for (var i = 1; i < n; i++) {
+    leftPref.push(leftPref[leftPref.length - 1] + BigInt(nums[i]));
+  } // Build right prefix sum
+
+
+  var rightPref = [BigInt(nums[n - 1])];
+
+  for (var _i = n - 2; _i >= 0; _i--) {
+    rightPref.push(rightPref[rightPref.length - 1] + BigInt(nums[_i]));
+  } // main algo to check for valid splits
+
+
+  rightPref.reverse();
+  var splitCounter = 0;
+
+  for (var _i2 = 0; _i2 < n - 1; _i2++) {
+    var prefLeft = leftPref[_i2];
+    var prefRight = rightPref[_i2 + 1];
+
+    if (prefLeft >= prefRight) {
+      splitCounter++;
+    }
+  }
+
+  return splitCounter;
+}; // approach: using the idea of general prefix left and right sums
+// another easy way is to calculate total and subtract from the end of things
+
+
+console.log(numberOfWaysToSplit([10, 4, -8, 7]));
---
+++ questions/general/numberOfWaysToSplit.js
@@ -0,0 +1,29 @@
+const numberOfWaysToSplit = (nums) => {
+  const n = nums.length;
+  // Build left prefix sum
+  let leftPref = [BigInt(nums[0])];
+  for (let i = 1; i < n; i++) {
+    leftPref.push(leftPref[leftPref.length - 1] + BigInt(nums[i]));
+  }
+  // Build right prefix sum
+  let rightPref = [BigInt(nums[n - 1])];
+  for (let i = n - 2; i >= 0; i--) {
+    rightPref.push(rightPref[rightPref.length - 1] + BigInt(nums[i]));
+  }
+  // main algo to check for valid splits
+  rightPref.reverse();
+  let splitCounter = 0;
+  for (let i = 0; i < n - 1; i++) {
+    const prefLeft = leftPref[i];
+    const prefRight = rightPref[i + 1];
+    if (prefLeft >= prefRight) {
+      splitCounter++;
+    }
+  }
+
+  return splitCounter;
+};
+
+// approach: using the idea of general prefix left and right sums
+// another easy way is to calculate total and subtract from the end of things
+console.log(numberOfWaysToSplit([10, 4, -8, 7]));
