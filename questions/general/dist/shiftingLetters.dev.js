b8a619cc29ddc3e384a8f163c36164b2aaca3265
RumonHasan <rumonuniv@gmail.com>
2025-01-05 21:10:53 +0900

    shift letters

--- questions/general/dist/shiftingLetters.dev.js
+++ questions/general/dist/shiftingLetters.dev.js
@@ -25,4 +25,80 @@ var shiftingLetters = function shiftingLetters(s, shifts) {
   return res;
 };
 
-console.log(shiftingLetters('abc', [3, 5, 9]));
+console.log(shiftingLetters('abc', [3, 5, 9])); // getting unique length three palindromes while making aure they are all unique strings
+
+var uniqueLengthThreePal = function uniqueLengthThreePal(s) {
+  var leftSet = new Set();
+  var map = new Map();
+  var set = new Set();
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = s[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var _char = _step.value;
+      map.set(_char, (map.get(_char) || 0) + 1);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = s[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var _char2 = _step2.value;
+      // remove the mid char
+      map.set(_char2, map.get(_char2) - 1);
+
+      if (map.get(_char2) === 0) {
+        map["delete"](_char2);
+      } // checking all the letters to see whether there is any on the right to match
+
+
+      for (var i = 97; i <= 122; i++) {
+        var checkChar = String.fromCharCode(i);
+
+        if (leftSet.has(checkChar) && map.get(checkChar) > 0) {
+          // only works if the left side is valid
+          set.add("".concat(checkChar).concat(_char2).concat(checkChar));
+        }
+      }
+
+      if (!leftSet.has(_char2)) {
+        leftSet.add(_char2);
+      }
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+        _iterator2["return"]();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+
+  return set.size;
+};
+
+console.log(uniqueLengthThreePal('aabca'));
--- questions/general/shiftingLetters.js
+++ questions/general/shiftingLetters.js
@@ -6,7 +6,6 @@ const shiftingLetters = (s, shifts) => {
   for (let i = shifts.length - 2; i >= 0; i--) {
     pref[i] = pref[i + 1] + shifts[i];
   }
-
   // have to change based on the total shifts from the end
   let res = '';
   for (let i = 0; i < s.length; i++) {
@@ -20,3 +19,34 @@ const shiftingLetters = (s, shifts) => {
 };
 
 console.log(shiftingLetters('abc', [3, 5, 9]));
+
+// getting unique length three palindromes while making aure they are all unique strings
+const uniqueLengthThreePal = (s) => {
+  let leftSet = new Set();
+  let map = new Map();
+  let set = new Set();
+  for (let char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (let char of s) {
+    // remove the mid char
+    map.set(char, map.get(char) - 1);
+    if (map.get(char) === 0) {
+      map.delete(char);
+    }
+    // checking all the letters to see whether there is any on the right to match
+    for (let i = 97; i <= 122; i++) {
+      const checkChar = String.fromCharCode(i);
+      if (leftSet.has(checkChar) && map.get(checkChar) > 0) {
+        // only works if the left side is valid
+        set.add(`${checkChar}${char}${checkChar}`);
+      }
+    }
+    if (!leftSet.has(char)) {
+      leftSet.add(char);
+    }
+  }
+  return set.size;
+};
+
+console.log(uniqueLengthThreePal('aabca'));
