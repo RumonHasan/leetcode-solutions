001b9f1b63ee5b9114c3a3bea9846bfbb161bc34
RumonHasan <rumonuniv@gmail.com>
2025-02-26 21:23:38 +0900

    maxBoxUnits

--- questions/general/minOperationsToMakePeriodicChunks.js
+++ questions/general/minOperationsToMakePeriodicChunks.js
@@ -19,4 +19,41 @@ const minOpPeriodicChunks = (word, k) => {
 };
 
 // have to form k periodic partitions then calculate the total frequency then subtracted with the chunks to find the number of operations needed
-console.log(minOpPeriodicChunks('leetcodeleet', 4));
+//console.log(minOpPeriodicChunks('leetcodeleet', 4));
+
+const maxBoxUnits = (boxTypes, truckSize) => {
+  let boxCount = 0;
+  let maxUnits = 0;
+
+  boxTypes.sort((a, b) => b[1] - a[1]);
+
+  for (let i = 0; i < boxTypes.length; i++) {
+    const currentUnits = boxTypes[i][1];
+    const currBoxCount = boxTypes[i][0];
+    const currTotalUnits = currentUnits * currBoxCount;
+
+    maxUnits += currTotalUnits;
+    boxCount += currBoxCount;
+
+    // if its more reduce adjust then remove
+    if (boxCount > truckSize) {
+      const adjustCount = boxCount - truckSize;
+      maxUnits -= adjustCount * currentUnits;
+      break;
+    }
+  }
+
+  return maxUnits;
+};
+
+console.log(
+  maxBoxUnits(
+    [
+      [5, 10],
+      [2, 5],
+      [4, 7],
+      [3, 9],
+    ],
+    10
+  )
+);
