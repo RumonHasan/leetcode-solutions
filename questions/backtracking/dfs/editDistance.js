ad63ee8bc5bbeeac6490253a2d4e2e089f2f3f18
RumonHasan <rumonuniv@gmail.com>
2025-06-29 20:57:27 +0900

    edit distance

---
+++ questions/backtracking/dfs/editDistance.js
@@ -0,0 +1,44 @@
+const editDistance = (word1, word2) => {
+  const cache = new Map();
+
+  const dfs = (indexOne, indexTwo) => {
+    const cacheKey = `${indexOne}-${indexTwo}`;
+
+    // base case where if one of reaches the end then return the other value by length - the original length
+    if (indexOne >= word1.length) {
+      return word2.length - indexTwo;
+    }
+
+    if (indexTwo >= word2.length) {
+      return word1.length - indexOne;
+    }
+    // return from cache
+    if (cache.has(cacheKey)) {
+      return cache.get(cacheKey);
+    }
+    // if the chars are equal then skip the current count
+    if (word1[indexOne] === word2[indexTwo]) {
+      return dfs(indexOne + 1, indexTwo + 1);
+    }
+    // only count the operation if the chars are not equal
+    let result = 0;
+    if (word1[indexOne] !== word2[indexTwo]) {
+      result =
+        1 +
+        Math.min(
+          dfs(indexOne + 1, indexTwo + 1),
+          dfs(indexOne + 1, indexTwo),
+          dfs(indexOne, indexTwo + 1)
+        );
+    }
+    cache.set(cacheKey, result);
+    return result;
+  };
+
+  return dfs(0, 0);
+};
+
+/**
+ * Using dfs memoization to insert remove and replace... only skip if the characters are already equal
+ */
+console.log(editDistance('intention', 'execution'));
